e1f70eba26f628460ca382e517bbc32735241f36
shuoshuo <shuoshuo@shuoshuodeMacBook-Pro.local>
2022-01-24 15:10:13 -0700

    update files

---
+++ src/components/Home.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import styled from "styled-components"
+import Section from "./Section"
+
+function Home() {
+    return (
+        <div>
+            <Container>
+                <Section
+                    title="Model S"
+                    description="Order Online for Touchless Delievery"
+                    backgroundImg="model-s.jpg"
+                    leftBtnText="Custom order"
+                    rightBtntext="Existing inventory"
+                />
+                <Section
+                    title="Model Y"
+                    description="Order Online for Touchless Delievery"
+                    backgroundImg="model-y.jpg"
+                    leftBtnText="Custom order"
+                    rightBtntext="Existing inventory"
+                />
+                <Section
+                    title="Model 3"
+                    description="Order Online for Touchless Delievery"
+                    backgroundImg="model-3.jpg"
+                    leftBtnText="Custom order"
+                    rightBtntext="Existing inventory"
+                />
+                <Section
+                    title="Model X"
+                    description="Order Online for Touchless Delievery"
+                    backgroundImg="model-x.jpg"
+                    leftBtnText="Custom order"
+                    rightBtntext="Existing inventory"
+                />
+                <Section
+                    title="Lowest Cost Solar Panels in America"
+                    description="Moeny-back guarantee"
+                    backgroundImg="solar-panel.jpg"
+                    leftBtnText="Order now"
+                    rightBtntext="Learn more"
+                />
+                <Section
+                    title="Solar for New Roots"
+                    description="Solar Roof Costs Less Than a New Rood Paneis"
+                    backgroundImg="solar-roof.jpg"
+                    leftBtnText="Order now"
+                    rightBtntext="Learn more"
+                />
+                <Section
+                    title="Accessories"
+                    description=""
+                    backgroundImg="accessories.jpg"
+                    leftBtnText="Shop now"
+                />
+            </Container>
+        </div>
+    )
+}
+
+export default Home;
+
+const Container = styled.div`
+    height: 100vh;
+
+`
